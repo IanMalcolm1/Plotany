f3c73b5c238772556bbf5dfed566c2f3d2f0e088
Revolution5 <damonboone99@gmail.com>
2025-07-26 09:46:24 -0700

    display plants based off soil type and climate (hard coded)

--- Plotany/PlantList.xaml.cs
+++ Plotany/PlantList.xaml.cs
@@ -1,23 +1,27 @@
+using AndroidX.Camera.Video;
 using Esri.ArcGISRuntime.Data;
 using Esri.ArcGISRuntime.Geometry;
 using Esri.ArcGISRuntime.Mapping;
+using System.Collections.ObjectModel;
 using System.Text;
+using static Android.Renderscripts.Script;
 
 namespace Plotany;
 
 public partial class PlantList : ContentPage
 {
-	public PlantList()
+    private ObservableCollection<string> resultItems = new ObservableCollection<string>();
+    public PlantList()
 	{
 		InitializeComponent();
-	}
+        ResultsCollection.ItemsSource = resultItems;
+    }
 
-	public async Task querySoilAtMapPoint()
+    public async Task<string> queryDataAtMapPoint(string url, string field, double x, double y)
 	{
-		string url = "https://landscape11.arcgis.com/arcgis/rest/services/USA_Soils_Map_Units/featureserver/0";
         var serviceFeatureTable = new ServiceFeatureTable(new Uri(url));
 
-        var mapPoint = new MapPoint(-118.805000, 34.027000, SpatialReferences.Wgs84); 
+        var mapPoint = new MapPoint(x, y, SpatialReferences.Wgs84); 
 
         // Create query parameters
         var queryParams = new QueryParameters
@@ -30,20 +34,63 @@ public partial class PlantList : ContentPage
         var result = await serviceFeatureTable.QueryFeaturesAsync(queryParams, QueryFeatureFields.LoadAll);
         var feature = result.FirstOrDefault();
 
-
-        if (feature.Attributes.TryGetValue("esrisymbology", out var value) && value != null)
+        if (feature.Attributes.TryGetValue(field, out var value) && value != null)
         {
-            await DisplayAlert("Soil Info", $"Soil Found: {value}", "OK");
+            await DisplayAlert("Info", $"Found: {value}", "OK");
+            return value.ToString();
+
         }
         else
         {
-            await DisplayAlert("Soil Info", "Not Found: no soil", "OK");
+            await DisplayAlert("Info", "Not Found: no data", "OK");
+            return "Not Found: no data";
         }
 
     }
 
-    private async void GetSoilOnClick(object sender, EventArgs e)
+    private async Task GetPlantList(string soilType, string climateType)
     {
-        await querySoilAtMapPoint();
+        var table = new ServiceFeatureTable(new Uri("https://services8.arcgis.com/LLNIdHmmdjO2qQ5q/arcgis/rest/services/Plant_data/FeatureServer/0"));
+        await table.LoadAsync(); // Required to access schema
+
+        string whereClause = $"Esri_Symbology = '{soilType}' AND Climate = '{climateType}'";
+
+        var queryParams = new QueryParameters
+        {
+            WhereClause = whereClause,
+            MaxFeatures = 1000
+        };
+
+        // Query all features and include all fields
+        var result = await table.QueryFeaturesAsync(queryParams, QueryFeatureFields.LoadAll);
+
+        if (!result.Any())
+        {
+            Console.WriteLine("No matching records found.");
+            return;
+        }
+
+        foreach (var feature in result)
+        {
+            string ID = feature.Attributes.TryGetValue("ID", out var mk) ? mk?.ToString() ?? "N/A" : "N/A";
+            string commonName = feature.Attributes.TryGetValue("Common_Name", out var cn) ? cn?.ToString() ?? "N/A" : "N/A";
+            string formatted = $"ID: {ID}, Common Name: {commonName}";
+            resultItems.Add(formatted);
+        }
+    }
+
+    private async void GetSoil(object sender, EventArgs e)
+    {
+        await queryDataAtMapPoint("https://landscape11.arcgis.com/arcgis/rest/services/USA_Soils_Map_Units/featureserver/0", "esrisymbology", -118.805000, 34.027000);
+    }
+
+    private async void GetClimate(object sender, EventArgs e)
+    {
+        await queryDataAtMapPoint("https://services7.arcgis.com/oF9CDB4lUYF7Um9q/arcgis/rest/services/NA_Climate_Zones/FeatureServer/5", "Climate", -118.805000, 34.027000);
+    }
+
+    private async void GetPlant(object sender, EventArgs e)
+    {
+        await GetPlantList("Aridisols", "Hot-Summer Mediterranean Climate");
     }
 }
