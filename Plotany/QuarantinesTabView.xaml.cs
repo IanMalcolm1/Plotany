2fc21f2206d177e56c1f67482d722440f9b4ecc7
Ian Malcolm <ian13774@esri.com>
2025-07-27 09:30:00 -0700

    Your garden now shows on the quarantines tab

--- Plotany/QuarantinesTabView.xaml.cs
+++ Plotany/QuarantinesTabView.xaml.cs
@@ -2,8 +2,12 @@ namespace Plotany;
 
 public partial class QuarantinesTabView : ContentPage
 {
-    public QuarantinesTabView()
+    private QuarantinesTabViewModel _viewModel;
+    public QuarantinesTabView(GardenManager gardenManager)
     {
         InitializeComponent();
+
+        _viewModel = new QuarantinesTabViewModel(gardenManager);
+        this.BindingContext = _viewModel;
     }
 }
